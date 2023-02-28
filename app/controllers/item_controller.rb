eabaf77cbf3bf41e16f0d3bc07e28dffa561129f
Adedotun Adepoju <adedotun.adepoju@b54.co>
2023-02-28 20:12:46 +0100

    add routes for category and items

---
+++ app/controllers/item_controller.rb
@@ -0,0 +1,24 @@
+class ItemController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def index
+    puts "not here"
+    # @items = Products.all
+  end
+
+  def fetch_item
+    puts params[:item_id]
+    # @item = Items.find(params[:item_id])
+  end
+
+  def create
+    # item = Item.new(params[:item])
+    # @category = Categories.find_by(slug: params[:item])
+    puts params
+    # @item
+  end
+
+  private
+  def item_params
+    params.permit(:item_name, :stock_number, :description, :price, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
-Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  
+  # category routes
+  post '/category/add', to: 'category#create'                   # Create category
+  get  '/category/:category_id', to "category#fetch_category"   # fetch one category
+  get  '/categories', to "category#fetch_all"                   # fetch all categories
+
+  # Item routes
+  get '/items', to: 'item#index'                 # fetch all items
+  get '/item/:item_id', to: "item#fetch_item"   # Fetch one item
+  post "/item", to: "item#create"                # Create new item
+
 end
