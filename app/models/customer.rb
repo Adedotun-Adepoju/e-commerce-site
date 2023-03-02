2b392049f88f20b75def86b5c8e112b688ed1eee
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-03 00:25:57 +0100

    add one-one relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,5 @@ class Customer < ApplicationRecord
     validates :first_name, presence: true # Validate first_name field
     validates :last_name, presence: true  # validate last_name field
     validates :email, presence: true    # validate email field
+    has_one :cart 
 end
