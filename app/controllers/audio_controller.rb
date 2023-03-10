02542a014f08d7f89d7c0c5efeafbfafa60e46c5
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-10 20:02:46 +0100

    add credentials to gitignore

--- app/controllers/audio_controller.rb
+++ app/controllers/audio_controller.rb
@@ -1,10 +1,17 @@
-# require "google/cloud/storage"
+require "google/cloud/storage"
 
 class AudioController < ApplicationController
     skip_before_action :verify_authenticity_token
     def upload_audio 
         #Initialize a google cloud storage with the service account JSON file
-        puts ENV.keys
-        puts ENV.fetch("GCS_CREDENTIALS")
+        project_id = ENV["PROJECT_ID"]
+        credentials = ENV["GCS_CREDENTIALS"]
+
+        storage = Google::Cloud::Storage.new(
+            project_id: project_id,
+            credentials: credentials
+        )
+        puts project_id
+        # Get the uploaded file from the params
     end
 end
