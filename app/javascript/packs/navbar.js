e7f1bdc372484f1f63a338612bd127d040b07dfa
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-10 18:19:03 +0100

    add feature to switch view for voice search

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -17,3 +17,27 @@
 //     const value = searchForm.querySelector('input[type="text"]').value 
 //     console.log(value)   
 // }
+
+const recordButton = document.getElementById('record-button');
+
+recordButton.addEventListener('click', (e) => {
+    const searchBar = e.target.parentElement
+    const wrapper = e.target.closest(".sub-banner");
+    const nav = wrapper.closest("nav")
+    
+    nav.removeChild(wrapper)
+    let speechNotification = document.createElement('div');
+    speechNotification.classList.add('speech-active')
+
+    let header = document.createElement("h2");
+    let headerText = document.createTextNode("Listening...")
+    header.classList.add('text')
+    header.append(headerText)
+
+    let img = document.createElement("img");
+    img.src = "https://icon-library.com/images/google-voice-search-icon/google-voice-search-icon-8.jpg"
+    
+    speechNotification.append(header, img);
+    nav.append(speechNotification);
+    console.log(nav)
+})
