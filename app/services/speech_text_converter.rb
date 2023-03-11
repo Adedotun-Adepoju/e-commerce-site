a622aa60bec4ad2ce3c88800304222eb8e0f5b8a
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-11 08:14:24 +0100

    implement translation for mp3 files

--- app/services/speech_text_converter.rb
+++ app/services/speech_text_converter.rb
@@ -1,5 +1,6 @@
 require "google/cloud/storage"
-# require "google/cloud/speech"
+require "google/cloud/speech/v2"
+require "google/cloud/speech"
 
 class SpeechTextConverter
     def initialize(file)
@@ -11,8 +12,8 @@ class SpeechTextConverter
     end
 
     def call
-        file_url = upload_to_gcs(@file)
-        puts "here #{file_url}"
+        object_name = upload_to_gcs(@file)
+        convert_to_text(object_name)
     end
 
     private 
@@ -33,8 +34,48 @@ class SpeechTextConverter
         # upload the file to the bucket
         bucket = storage.bucket bucket, skip_lookup: true
         file = bucket.create_file(audio_file.tempfile, object_name)
-        return file.public_url
+        return object_name
     end
 
-    def save_reference(object_name, url) end
+    def convert_to_text(object_name) 
+        project_id = ENV["PROJECT_ID"]
+        credentials = ENV["SPEECH_CREDENTIALS"]
+        bucket = ENV["GCS_BUCKET_NAME"]       
+
+        speech = Google::Cloud::Speech.speech
+        audio_uri = "gs://#{bucket}/#{object_name}"
+        puts audio_uri
+        config = {
+            language_code: "en-us",
+            encoding: :FLAC,
+        }
+        audio = { 
+            uri: audio_uri 
+        }
+        request = ::Google::Cloud::Speech::V1::RecognizeRequest.new(
+            config: {
+                language_code: "en-us",
+                encoding: :LINEAR16,
+            }, 
+            audio: {
+                uri: audio_uri                 
+            })
+        # response = speech.recognize config:config, audio: audio
+        response = speech.recognize request
+        puts "res #{response}"
+
+        transcript = "a"
+        response.results.each do |result|
+            puts result
+            transcript += result.alternatives[0].transcript
+        end 
+
+        puts "here"
+
+        puts "transcript, #{transcript}"
+    end
+    
+    def save_reference(object_name, url) 
+    
+    end
 end
