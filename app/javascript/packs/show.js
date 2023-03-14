210abd935ae4e9ffb1b38d1d3b98c51e5ed290fa
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-15 00:45:08 +0100

    modify show.js

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,5 +1,8 @@
 const actionButton = document.getElementById("add-button")
 
+
 actionButton.addEventListener('click', (e) => {
-    console.log(e)
+   console.log("here")
+   const id = e.target.getAttribute("data-id")
+   console.log(id);
 })
