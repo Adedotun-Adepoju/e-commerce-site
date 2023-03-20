08739c29c56694c874fa6dba90759913be75fdf8
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-20 23:32:19 +0100

    remove comments

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -1,23 +1,3 @@
-// const voiceSearch = document.querySelector('.voice-image')
-
-// voiceSearch.addEventListener('click', (e)=>{
-//     console.log(e.target)
-// })
-
-// const searchForm = document.forms["search-products"]
-
-// searchForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const value = searchForm.querySelector('input[type="text"]').value 
-//     console.log(value)
-// })
-
-// function search(){
-//     const value = searchForm.querySelector('input[type="text"]').value 
-//     console.log(value)   
-// }
-
 const recordButton = document.getElementById('record-button');
 let recording = false;
 let mediaRecorder;
