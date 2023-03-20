8b031f58f3bef404d2818448bef9ca44d7648f24
Adedotun Adepoju <d.e.adepoju@gmail.com>
2023-03-20 22:51:44 +0100

    remove action to fetch categories

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,28 +1,9 @@
 class CategoryController < ApplicationController
   skip_before_action :verify_authenticity_token
+  # action to add new category
   def create
-    puts "create"
-
     # save all categories to the db
     @categories = Category.create(category_params)
-  end
-
-  def fetch_category 
-    id = params[:category_id]
-
-    # find the category with the specified id
-    @category = Category.find(id)
-
-    puts @category.slug
-  end 
-
-  def fetch_all 
-    # fetch all categorie
-    @categories = Category.all 
-    
-    for category in @categories
-      puts category.slug
-    end
   end
 
   private 
